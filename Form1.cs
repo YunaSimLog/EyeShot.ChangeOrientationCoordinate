15e023935baba474bec2d28393d9ef424fc95b9c
yunasim <ynsim@solidit.co.kr>
2024-04-20 15:29:40 +0900

    #✨feature: 소스 업로드
    기능 구현 완료됨

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TransformCoordinate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpZ_Click(object sender, EventArgs e)
+        {
+            design1.OrientationMode = devDept.Graphics.orientationType.UpAxisZ;
+            design1.SetView(devDept.Eyeshot.viewType.Isometric);
+            design1.Invalidate();
+        }
+
+        private void btnUpY_Click(object sender, EventArgs e)
+        {
+            design1.OrientationMode = devDept.Graphics.orientationType.UpAxisY;
+            design1.SetView(devDept.Eyeshot.viewType.Isometric);
+            design1.Invalidate();
+        }
+    }
+}
